4740942feb96697710426316c3222c3cb2d89e4b
natthapongnks <124965869+natthapongnks@users.noreply.github.com>
2026-08-18 15:25:44 +0700

    js

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -44,7 +44,7 @@
         @viteReactRefresh
         @vite(['resources/css/app.css', 'resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
         <x-inertia::head>
-            <title>{{ config('app.name', 'Laravel') }}</title>
+            <title>{{ config('app.name', 'JS Sport') }}</title>
         </x-inertia::head>
     </head>
     <body class="font-sans antialiased">
